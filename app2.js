569da5a3472ab27f3b916aa3269dbcab86b4b172
cosil2b2 <53854070+cosil2b2@users.noreply.github.com>
2020-08-09 22:08:30 +0900

    pugTemplate and QueryString

--- app2.js
+++ app2.js
@@ -1,11 +1,65 @@
 const express = require("express");
-const { Router } = require("express");
 const app = express();
+app.set("view engine", "pug");
+app.set("views", "./views");
 const port = 3000;
 
 app.use(express.static("public"));
 //이거 쓰면 정적 파일을 보낼 수 있음. 주소/.파일명.파일형식
 //public 디렉토리 안에 있어야 함.
+
+/**
+ * 시멘틱URL
+ * semantic URL
+ * /topic/1 이런식으로 깔끔하게 보여주는 것
+ *
+ * router에서 경로를
+ * /topic   ==>   /topic/:id
+ * 으로 해줘야 함.
+ */
+app.get("/semantic/:id", (req, res) => {
+  // req객체의 query객체안의 queryString 객체들을 가져옴.
+  const topics = [
+    "Javascript is ......",
+    "Nodejs is .......",
+    "Express is ........",
+  ];
+  const output = `
+  <a href="/semantic/0">JavaScript</a><br>
+  <a href="/semantic/1">Nodejs</a><br>
+  <a href="/semantic/2">Express</a><br><br>
+  ${topics[req.params.id]}`;
+  //시멘틱 url의 경우 params로 적어야함.
+  res.send(output);
+});
+//이렇게 두개인 경우에도 처리할 수 있음.
+app.get("/semantic/:id/:mode", (req, res) => {
+  res.send(req.params.id + "," + req.params.mode);
+});
+
+//queryString -> 요청부분인 req를 사용하게 될 것임.
+app.get("/topic", (req, res) => {
+  // req객체의 query객체안의 queryString 객체들을 가져옴.
+  const topics = [
+    "Javascript is ......",
+    "Nodejs is .......",
+    "Express is ........",
+  ];
+  const output = `
+  <a href="/topic?id=0">JavaScript</a><br>
+  <a href="/topic?id=1">Nodejs</a><br>
+  <a href="/topic?id=2">Express</a><br><br>
+  ${topics[req.query.id]}`;
+  res.send(output);
+  //시멘틱 URL에서 이어서...
+});
+
+app.get("/template", (req, res) => {
+  //  pug 에서 정했던 이름에다가 글 지정함.
+  app.locals.pretty = true; //view-source:주소 로 볼 때 깨끗하게 보여주는 코드
+  const time = Date();
+  res.render("temp", { title: "MyProject", time: time });
+});
 
 app.get("/", (req, res) => {
   res.send("/index.html");
@@ -53,4 +107,6 @@ app.listen(port, () => {
  */
 
 //정적인 장점, 동적인 장점 다 맞는거 어떻게 할까 ?
-//--> 템플릿 엔진 (in express js) ex) jade, pug
+//--> 템플릿 엔진 (in express js) ex) pug
+
+//temp.pug에서 이어서
